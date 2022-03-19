933428aa547428ad4407345190388929072de5ea
M. Rehan <mrehanlm93@gmail.com>
2022-03-19 15:45:12 +0500

    Add ability to remove unused containers/images

--- src/middlewared/middlewared/plugins/docker_linux/utils.py
+++ src/middlewared/middlewared/plugins/docker_linux/utils.py
@@ -1,3 +1,4 @@
+import docker
 import re
 from typing import Dict
 
@@ -59,3 +60,7 @@ def normalize_reference(reference: str) -> Dict:
         'complete_tag': f'{registry}/{image}{sep}{tag}',
         'reference_is_digest': ref_is_digest,
     }
+
+
+def get_docker_client() -> docker.DockerClient:
+    return docker.from_env()
